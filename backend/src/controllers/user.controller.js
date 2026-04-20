63247f0069f7c97b0d7d729e4de2ad2068107df3
NoahRod108 <noah.rodriguez.portfolio@gmail.com>
2026-04-20 11:13:45 -0500

    Finished adding auth api endpoints

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -61,8 +61,24 @@ const loginUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error!" });
+    res.status(500).json({ message: "Internal server error!", error });
   }
 };
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    res.status(200).json({ message: "User found: logout successful!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error: ", error });
+  }
+};
+
+export { registerUser, loginUser, logoutUser };
