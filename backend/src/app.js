64fd20423c25c9bd41ef8e365ad2540e85653584
NoahRod108 <noah.rodriguez.portfolio@gmail.com>
2026-05-05 12:01:18 -0500

    Preparing project for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express(); //Create an express app
 
 // Parse json req from users
 app.use(express.json());
 // Middleware to show frontend
-app.use(express.static("./frontend"));
+app.use(express.static(path.join(__dirname, "../../frontend")));
 
 // routes import
 import userRouter from "./routes/user.route.js";
